a25573bb62b27f4bc6893801f7180b8a26578205
Saharat Thongin <was.dew4546@gmail.com>
2020-09-21 18:01:43 +0700

    Initial Commit

---
+++ OnlineSalesSystem/OnlineSales.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+
+// Data Storage
+string name[99];
+string surname[99];
+
+//function declare
+
+
+int main() {
+	cout << " >> Welcome to www.onlinemarketing.co.th <<" << endl << endl;
+	cout << "please select menu" << endl;
+	
+	
+	system("pause");
+	return 0;
+}
