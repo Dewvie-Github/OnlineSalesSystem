58f2a498b26d8e666371f85c0d4100958717289e
Saharat Thongin <was.dew4546@gmail.com>
2020-09-21 19:24:47 +0700

    add selected menu

--- OnlineSalesSystem/OnlineSales.cpp
+++ OnlineSalesSystem/OnlineSales.cpp
@@ -6,13 +6,23 @@ using namespace std;
 // Data Storage
 string name[99];
 string surname[99];
+bool exitProgram = false;
+char menu;
 
 //function declare
 
 
 int main() {
-	cout << " >> Welcome to www.welcome mabg.co.th <<" << endl << endl;
-	cout << "please select menu" << endl;
+	cout << " >> Welcome to Online Warehouse Management <<" << endl << endl;
+	do {
+		cout << "Please selected menu" << endl;
+		cout << "1. Manage Customer Data." << endl;
+		cout << "2. Manage Stockpile." << endl;
+		cout << "3. Sell Product." << endl;
+		cout << "4. Exit." << endl;
+		cout << "Input menu : " << endl;
+		cin >> menu;
+	} while (exitProgram != true);
 
 	
 	
