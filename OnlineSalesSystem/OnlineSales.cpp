d28764f1dd8b07840748717e01c6ced583c6451b
Saharat Thongin <was.dew4546@gmail.com>
2020-10-19 15:19:24 +0700

    Finished Customer Data Input

--- OnlineSalesSystem/OnlineSales.cpp
+++ OnlineSalesSystem/OnlineSales.cpp
@@ -1,53 +1,112 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<cstdlib>
+
+
+
 using namespace std;
 
-// admin user and password
+// admin username and password
 string adminUsername = "admin",adminPassword = "1234";
 
 
-// Customer datas
+// Customer Datas
+int customerCount = 0; //This is for count data(data).
 int customerID[99];
-string customerName[99];
+int idGenerate = 630000;
+string customerName[99][2];
 string customerTel[99];
 string customerEmail[99];
 
-// Warehouse datas
-string productName[99], productClass[99];
-int productType[99], productPrice[99], productQuantity[99];
+// Warehouse Datas
+string productName[99], productClass[99], productType[99];
+int idCount = 0;
+int productPrice[99], productQuantity[99];
 
-// Selling system datas
+// Selling System Datas
 string checkID;
 
-
-
 bool exitProgram = false;
-char menu;
-
+char mainMenu, menu;
 
 //function declare
-char showMenu(char &menu);
+char showMainMenu(char &mainMenu);
+
 
 int main() {
+	
+	
 	cout << " >> Welcome to Online Warehouse Management <<" << endl << endl;
 	do {
-		showMenu(menu);
-		
-		if (menu == '1') {
+		showMainMenu(mainMenu);
+		if (mainMenu == '1') {
+			do {
+				cout << "=======================================" << endl;
+				cout << "[ Manage Customer Data ]" << endl;
+				cout << "(1) Register" << endl;
+				cout << "(2) Manage ID" << endl;
+				cout << "(3) Show all ID. [admin only]" << endl;
+				cout << "(4) Exit to main menu." << endl;
+				cout << "---------------------------------------" << endl;
+				cout << " >> Enter menu : ";
+				cin >> menu;
+				
+				if (menu == '1') {
+					cout << endl << "Register..." << endl;
+
+					int i = customerCount;
+					cout << "Enter name surname (Example : Farsang Jangsai) : " << endl;
+					cout << " >> ";
+					cin >> customerName[i][0] >> customerName[i][1];
+
+					cout << "Enter your telephone" << endl;
+					cout << " >> ";
+					cin >> customerTel[i];
+
+					cout << "Enter your email " << endl;;
+					cout << " >> ";
+					cin >> customerEmail[i];
+					
+					customerID[i] = ++idGenerate;
+
+					customerCount++;
+
+					cout << "-----------------------------------------------------------------" << endl;;
+					cout << "	ID : " << customerID[i] << endl;
+					cout << "	Name : " << customerName[i][0] << " " << customerName[i][1] << endl;
+					cout << "	Tel. " << customerTel[i] << " " << "Email : " << customerEmail[i] << endl;
+					cout << "-----------------------------------------------------------------" << endl << endl;
+				}
+				else if (menu == '2') {
+
+				}
+				else if (menu == '3') {
+
+				}
+				else if (menu == '4') {
+					cout << "Exit to main menu." << endl;
+					cout << "=======================================" << endl;
+				}
+				else {
+					cout << endl << ">> invalid input! Please enter again <<" << endl;
+				}
+				
+			} while (menu != '4');
+			
 			
 		}
-		else if (menu == '2') {
+		else if (mainMenu == '2') {
 
 		}
-		else if (menu == '3') {
+		else if (mainMenu == '3') {
 
 		}
-		else if (menu == '4') {
+		else if (mainMenu == '4') {
 			char check;
 			do {
 				cout << "Please confirm you exit status(y/n)";
-				cin >> check ;
+				cin >> check;
 				if (check == 'y' || check == 'Y') {
 					cout << "Exit Program..." << endl;
 					exitProgram = true;
--- OnlineSalesSystem/showMenu.cpp
+++ OnlineSalesSystem/showMenu.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
-char showMenu(char &menu){
+char showMainMenu(char &menu){
 	cout << "=======================================" << endl;
 	cout << "Please selected menu" << endl;
 	cout << "1) Manage Customer Data." << endl;
 	cout << "2) Manage Stockpile." << endl;
 	cout << "3) Sell Product." << endl;
 	cout << "4) Exit." << endl;
-	cout << "=======================================" << endl;
+	cout << "---------------------------------------" << endl;
 	cout << ">> Enter menu : ";
 	cin >> menu;
+	cout << endl;
 	return 0;
 }
